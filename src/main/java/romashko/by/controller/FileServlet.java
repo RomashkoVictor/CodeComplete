1a6abe90f75f4ee8f78bd35adde0be7997d2cad3
Victor <phantom.rvr@gmail.com>
2016-05-01 17:55:32 +0300

    Add Post request

---
+++ src/main/java/romashko/by/controller/FileServlet.java
@@ -0,0 +1,35 @@
+package romashko.by.controller;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@WebServlet(urlPatterns = {"/test"})
+public class FileServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int num = Integer.parseInt(request.getParameter("num"));
+        String checkSum = request.getParameter("checksum");
+        String data = new String(Base64.getDecoder().decode(request.getParameter("data")), StandardCharsets.UTF_8);
+        boolean isEnd = Boolean.parseBoolean(request.getParameter("isend"));
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter out = response.getWriter();
+        if (DigestUtils.md5Hex(data).equals(checkSum)) {
+            out.print("OK");
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            out.print("REPEAT");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+        out.flush();
+    }
+}
